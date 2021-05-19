41f8b0ac333d9493310ddfa2eef7a893b983defd
LeeOhHyung <lee2928030@naver.com>
2021-05-19 15:38:58 +0900

    [feat] 바텀 네이게이션 뷰의 초기 탭 설정 작업 추가

--- app/src/main/kotlin/kr/tooni/tooni/features/MainActivity.kt
+++ app/src/main/kotlin/kr/tooni/tooni/features/MainActivity.kt
@@ -2,9 +2,12 @@ package kr.tooni.tooni.features
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import kr.tooni.tooni.R
 import kr.tooni.tooni.base.BaseActivity
 import kr.tooni.tooni.databinding.ActivityMainBinding
+import kr.tooni.tooni.features.day.DayWebtoonFragment
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     
@@ -13,7 +16,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
-        subscribe()
+        setInitialTab()
+        setBottomNavigationItemSelectedListener()
     }
     
     private fun initView() {
@@ -21,6 +25,55 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         binding.lifecycleOwner = this
     }
     
-    private fun subscribe() {
+    private fun setInitialTab() {
+        showPhotoFragment()
     }
+    
+    private fun setBottomNavigationItemSelectedListener() {
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home_1 -> showPhotoFragment()
+                R.id.home_2 -> showPhotoFragment()
+                R.id.home_3 -> showPhotoFragment()
+                R.id.home_4 -> showPhotoFragment()
+                else -> throw IllegalArgumentException("${menuItem.itemId} is invalid itemId")
+            }
+        
+            return@setOnNavigationItemSelectedListener true
+        }
+    }
+    
+    private fun showPhotoFragment() {
+        val tag = DayWebtoonFragment::class.java.name
+        
+        supportFragmentManager.findFragmentByTag(tag)
+            ?.let { fragment ->
+                showFragment(fragment)
+            }
+            ?: addFragment(
+                fragment = DayWebtoonFragment.newInstance(),
+                tag = tag
+            )
+    }
+    
+    private fun addFragment(fragment: Fragment, tag: String) =
+        supportFragmentManager
+            .beginTransaction()
+            .hideFragments()
+            .add(R.id.container, fragment, tag)
+            .commitAllowingStateLoss()
+    
+    private fun showFragment(fragment: Fragment) =
+        supportFragmentManager
+            .beginTransaction()
+            .hideFragments()
+            .show(fragment)
+            .commitAllowingStateLoss()
+    
+    private fun FragmentTransaction.hideFragments(): FragmentTransaction =
+        this.apply {
+            supportFragmentManager
+                .fragments
+                .forEach { fragment -> hide(fragment) }
+        }
 }
---
+++ app/src/main/kotlin/kr/tooni/tooni/features/day/DayWebtoonViewModel.kt
@@ -0,0 +1,9 @@
+/*
+ * Created by Leo on 2021. 05. 19 ..
+ */
+package kr.tooni.tooni.features.day
+
+import kr.tooni.tooni.base.BaseViewModel
+
+class DayWebtoonViewModel : BaseViewModel() {
+}
