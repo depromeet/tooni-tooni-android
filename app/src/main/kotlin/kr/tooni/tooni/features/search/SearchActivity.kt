054222567a3d11e620d69f7f7ec4746c6c95da43
leejungmin05 <74166576+leejungmin05@users.noreply.github.com>
2021-05-19 18:02:05 +0900

    add feature/search layout

---
+++ app/src/main/kotlin/kr/tooni/tooni/features/search/SearchActivity.kt
@@ -0,0 +1,9 @@
+package kr.tooni.tooni.features.search
+
+import kr.tooni.tooni.R
+import kr.tooni.tooni.base.BaseActivity
+import kr.tooni.tooni.databinding.ActivitySearchBinding
+
+class SearchActivity: BaseActivity<ActivitySearchBinding>(R.layout.activity_search) {
+
+}
