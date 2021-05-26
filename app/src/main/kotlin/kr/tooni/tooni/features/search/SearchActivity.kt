a68081135f4313299fcb27b8c09ad3d4addd2142
leejungmin05 <74166576+leejungmin05@users.noreply.github.com>
2021-05-26 14:29:42 +0900

    add recommend recyclerview layout

--- app/src/main/kotlin/kr/tooni/tooni/features/search/SearchActivity.kt
+++ app/src/main/kotlin/kr/tooni/tooni/features/search/SearchActivity.kt
@@ -1,9 +1,18 @@
 package kr.tooni.tooni.features.search
 
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import kr.tooni.tooni.R
 import kr.tooni.tooni.base.BaseActivity
 import kr.tooni.tooni.databinding.ActivitySearchBinding
 
 class SearchActivity: BaseActivity<ActivitySearchBinding>(R.layout.activity_search) {
+    lateinit var mbinding: ActivitySearchBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mbinding = DataBindingUtil.setContentView(this,R.layout.activity_search)
+        mbinding.lifecycleOwner = this
+    }
 
 }
