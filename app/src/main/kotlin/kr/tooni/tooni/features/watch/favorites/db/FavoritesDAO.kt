6594d7cec256138790a9a9a25f2d6e45c18774a8
hyunjung-choi <hjchoi.dev@gmail.com>
2021-05-27 01:45:39 +0900

    [feat] FavoritesDAO 수정

--- app/src/main/kotlin/kr/tooni/tooni/features/watch/favorites/db/FavoritesDAO.kt
+++ app/src/main/kotlin/kr/tooni/tooni/features/watch/favorites/db/FavoritesDAO.kt
@@ -1,24 +1,28 @@
 package kr.tooni.tooni.watch.favorites.db
 
 import androidx.room.*
+import kotlinx.coroutines.flow.Flow
 import kr.tooni.tooni.features.watch.favorites.Favorites
 
 @Dao
 interface FavoritesDAO {
 
+    // Return the row id
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertFavorites(favorites : Favorites) : Long
 
+    // Retrun the number of rows that were updated
     @Update
-    suspend fun updateFavorites(favorites: Favorites)
+    suspend fun updateFavorites(favorites: Favorites) : Int
 
+    // Retrun the number of rows that were deleted
     @Delete
-    suspend fun deleteFavorites(favorites: Favorites)
+    suspend fun deleteFavorites(favorites: Favorites) : Int
 
+    // Retrun the number of rows that were deleted
     @Query("DELETE FROM favorites_data_table")
-    suspend fun deleteAllFavorites()
+    suspend fun deleteAllFavorites() : Int
 
     @Query("SELECT * FROM favorites_data_table")
-    fun getAllFavorites()
-
+    fun getAllFavorites() : Flow<List<Favorites>>
 }
