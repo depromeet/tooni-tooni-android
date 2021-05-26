ae294548b3d21bb72c8b236399bda6d5ab646f59
hyunjung-choi <hjchoi.dev@gmail.com>
2021-05-27 02:05:55 +0900

    [feat] Room Database Class 생성

---
+++ app/src/main/kotlin/kr/tooni/tooni/features/watch/favorites/db/FavoritesDatabase.kt
@@ -0,0 +1,32 @@
+package kr.tooni.tooni.features.watch.favorites.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import kr.tooni.tooni.features.watch.favorites.Favorites
+import kr.tooni.tooni.watch.favorites.db.FavoritesDAO
+
+@Database(entities = [Favorites::class], version = 1)
+abstract class FavoritesDatabase : RoomDatabase() {
+
+    abstract val favoritesDAO : FavoritesDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE : FavoritesDatabase? = null
+        fun getInstance(context: Context) : FavoritesDatabase {
+            synchronized(this) {
+                var instance : FavoritesDatabase? = INSTANCE
+                if(instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        FavoritesDatabase::class.java,
+                        "favorites_data_database"
+                    ).build()
+                }
+                return instance
+            }
+        }
+    }
+}
