58df6c23614d2717d2bf7b6ee604de9ab20d3e57
leejungmin05 <74166576+leejungmin05@users.noreply.github.com>
2021-05-31 23:12:08 +0900

    add feature 'show recent contraint layout

--- app/src/main/kotlin/kr/tooni/tooni/features/search/SearchActivity.kt
+++ app/src/main/kotlin/kr/tooni/tooni/features/search/SearchActivity.kt
@@ -3,6 +3,7 @@ package kr.tooni.tooni.features.search
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import kr.tooni.tooni.R
@@ -14,7 +15,7 @@ class SearchActivity: BaseActivity<ActivitySearchBinding>(R.layout.activity_sear
     var imm: InputMethodManager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mbinding = DataBindingUtil.setContentView(this,R.layout.activity_search)
+        mbinding = DataBindingUtil.setContentView(this, R.layout.activity_search)
         mbinding.lifecycleOwner = this
 
         val recentAdapter = RecentAdapter()
@@ -22,24 +23,27 @@ class SearchActivity: BaseActivity<ActivitySearchBinding>(R.layout.activity_sear
         mbinding.recentKeywordItem.layoutManager = LinearLayoutManager(this)
 
         imm = getSystemService(android.content.Context.INPUT_METHOD_SERVICE) as InputMethodManager?
-
-//        mbinding.searchHint.setOnClickListener{
-//            searchHintOnClick()
-//        }
+        
     }
 
-    fun searchHintOnClick() {
-        mbinding.searchView1.visibility == View.GONE
-        mbinding.searchView2.visibility == View.VISIBLE
-    }
 
     fun hideKeyboard(v: View) {
-        if(v!=null) {
-            imm?.hideSoftInputFromWindow(v.windowToken, 0 )
-        }
+        imm?.hideSoftInputFromWindow(v.windowToken, 0 )
+        showHide(mbinding.searchView2)
+        showHide(mbinding.searchView1)
     }
 
     fun showKeyboard(v: View){
         imm?.showSoftInput(mbinding.searchHint,0)
+        showHide(mbinding.searchView2)
+        showHide(mbinding.searchView1)
+    }
+
+    fun showHide(view: View) {
+        view.visibility = if(view.visibility == View.GONE){
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
     }
 }
