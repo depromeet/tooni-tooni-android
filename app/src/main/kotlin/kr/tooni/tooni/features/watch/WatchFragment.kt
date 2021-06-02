eb7dcaab0976413ea3068db8ea0104ff3b288613
hyunjung-choi <hjchoi.dev@gmail.com>
2021-06-02 12:36:59 +0900

    [feat] Toolbar 구현 및  Search menu 클릭 시 Search Activity 연결

--- app/src/main/kotlin/kr/tooni/tooni/features/watch/WatchFragment.kt
+++ app/src/main/kotlin/kr/tooni/tooni/features/watch/WatchFragment.kt
@@ -1,13 +1,13 @@
 package kr.tooni.tooni.features.watch
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayoutMediator
 import kr.tooni.tooni.R
 import kr.tooni.tooni.base.BaseFragment
 import kr.tooni.tooni.databinding.FragmentWatchBinding
+import kr.tooni.tooni.features.search.SearchActivity
 import kr.tooni.tooni.features.watch.adapter.WatchFragmentAdapter
 import kr.tooni.tooni.features.watch.favorites.FavoritesFragment
 import kr.tooni.tooni.features.watch.recent.RecentFragment
@@ -30,6 +30,8 @@ class WatchFragment : BaseFragment<FragmentWatchBinding>(R.layout.fragment_watch
         savedInstanceState: Bundle?
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
+        setupToolbar()
+
         // Fragments
         val fragmentsList = listOf(RecentFragment.newInstance(), FavoritesFragment.newInstance())
         val adapter = WatchFragmentAdapter(this)
@@ -41,5 +43,28 @@ class WatchFragment : BaseFragment<FragmentWatchBinding>(R.layout.fragment_watch
             tab.text = tabTitles[position]
         }.attach()
         return binding.root
+    }
+
+    private fun setupToolbar() {
+        setHasOptionsMenu(true)
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbarWatch)
+        val supportActionBar = (activity as AppCompatActivity).supportActionBar
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    // menu.search를 Toolbar에 적용
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        binding.toolbarWatch.menu.clear()
+        binding.toolbarWatch.inflateMenu(R.menu.search)
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    // SearchActivity로 이동
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_search -> SearchActivity.start(requireContext())
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
