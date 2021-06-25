85240311d3974a0a559bad090304268f6c2f4db3
hyunjung-choi <hjchoi.dev@gmail.com>
2021-06-25 22:26:48 +0900

    [feat] Splash 화면 구현 및 애니메이션 효과 추가 (#36)
    * [feat] Splash 화면 레이아웃 구현
    
    * [feat] Splash Activity에 애니메이션 적용
    
    * [fix] 애니메이션 끝까지 안 보이던 버그 수정

--- app/src/main/kotlin/kr/tooni/tooni/features/launcher/LauncherActivity.kt
+++ app/src/main/kotlin/kr/tooni/tooni/features/launcher/LauncherActivity.kt
@@ -4,6 +4,8 @@
 package kr.tooni.tooni.features.launcher
 
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import kr.tooni.tooni.R
@@ -16,27 +18,52 @@ import kr.tooni.tooni.features.launcher.LauncherViewModel.State.Success
 
 @AndroidEntryPoint
 class LauncherActivity : BaseActivity<ActivityLauncherBinding>(R.layout.activity_launcher) {
-    
+
     private val viewModel by viewModels<LauncherViewModel>()
-    
+    lateinit var ani: Animation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.viewModel = viewModel
+        ani = AnimationUtils.loadAnimation(this, R.anim.splash_stars)
+        binding.ivSplashStar1.startAnimation(ani)
+        binding.ivSplashStar2.startAnimation(ani)
+
         observe()
     }
-    
+
     private fun observe() {
         viewModel.state.observe(this) { state ->
             when (state) {
                 is Error -> showSnackbar(binding.root, state.errorMessage)
-                Success -> handleSuccessState()
+                Success -> {
+                    if(ani.hasEnded()) {
+                        handleSuccessState()
+                    } else {
+                        aniListener()
+                    }
+                }
             }
         }
     }
-    
+
     private fun handleSuccessState() {
         MainActivity.start(this) {
             finish()
         }
+    }
+
+    private fun aniListener() {
+        ani.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                MainActivity.start(this@LauncherActivity) {
+                    finish()
+                }
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
     }
 }
